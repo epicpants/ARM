60584e274b866a7f3f3ae7c09c392909176baeb7
epicpants <tjrg88@mst.edu>
2015-05-08 14:58:33 -0500

    Removed prototypes for unused functions in timer.h

--- TWI/timer.h
+++ TWI/timer.h
@@ -9,13 +9,8 @@
 #define T_FREQ (41.7334E-9)
 #include "types.h"
 
-//This function will initialize timer 0 to generate a 40 kHz square wave.
-extern void TMR_INIT(void);
-extern void START_TIMERS(void);
-extern void STOP_TIMERS(void);
-extern uint32 READ_TC2_SR(void);
-extern uint32 READ_TC2_REGISTER(uint32 register_mask);
 
+extern void STOP_TIMERS(void);
 
 
 #endif
